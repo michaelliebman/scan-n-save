e7abe38f9316329e80bcee886b0bea72e43730c8
Michael S. Liebman <msl521@users.sourceforge.net>
2013-07-29 21:06:58 -0400

    Issue 1: Fix crash when an intent receiver isn't available.
    * src/com/github/msl521/scannsave/MainActivity.java:
      (scanBarcode): Test if an intent receiver is available. If not, warn
      the user.

--- src/com/github/msl521/scannsave/MainActivity.java
+++ src/com/github/msl521/scannsave/MainActivity.java
@@ -1,13 +1,18 @@
 package com.github.msl521.scannsave;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
 	public final static String EXTRA_MESSAGE = "com.github.msl521.scannsave.MESSAGE";
 
 	@Override
@@ -22,27 +27,35 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void scanBarcode(View view) {
-		 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-	        intent.setPackage("com.google.zxing.client.android");
-	        //intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-	        startActivityForResult(intent, 0);
-	}
-	@Override
-	  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == 0) {
-	        if (resultCode == RESULT_OK) {
-	            String contents = intent.getStringExtra("SCAN_RESULT");
-	            // String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-	            Intent intentResult = new Intent(this, AnnotateScanActivity.class);
-	    		intentResult.putExtra(EXTRA_MESSAGE, contents);
-	    		startActivity(intentResult);
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // Handle cancel
-	        }
-	    }
-	  }
 
+	public void scanBarcode(View view) {
+		PackageManager pm = getPackageManager();
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		List<ResolveInfo> infos = pm.queryIntentActivities(intent, 0);
+		if (infos.size() > 0) {
+			intent.setPackage("com.google.zxing.client.android");
+			// intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			startActivityForResult(intent, 0);
+			
+		} else {
+			Toast.makeText(this, "No barcode scanner available.", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				// String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				Intent intentResult = new Intent(this,
+						AnnotateScanActivity.class);
+				intentResult.putExtra(EXTRA_MESSAGE, contents);
+				startActivity(intentResult);
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+			}
+		}
+	}
 
 }
